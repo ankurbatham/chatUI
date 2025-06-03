758b34c2ac03c3916c0157b504647e70b04f6020
TeenaPal <pteena495@gmail.com>
2025-06-03 08:29:33 +0530

    Adding Side button to open chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,72 @@
-import React, { useState } from 'react'
+import React, { useRef, useState,useEffect } from 'react'
 import Chatbot_icon from './components/Chatbot_icon'
 import Chatfooter from './components/Chatfooter'
 import ChatMessage from './components/ChatMessage'
 const App = () => {
   const[chatHistory , setChatHistory] = useState([])
+  const[chatOpen , setchatOpen] = useState(false)
+  const messageEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+  if (messageEndRef.current) {
+    messageEndRef.current.scrollIntoView({ behavior: "smooth" ,display:"none"});
+  }
+};
+
+useEffect(() => {
+  scrollToBottom();
+}, [chatHistory]);
+
+
   return (
-    <div>
-     <div className="container">
-      <div className="chat-popup">
-        <div className="chat-header">
+  <>
+    <div className="icon">
+      <button onClick={() => setchatOpen(!chatOpen)}> 
+        <Chatbot_icon />
+      </button>
+    </div>
 
-          <div className="header-info">
-            <Chatbot_icon/>
-            <h2 className='logo'>Chatbot</h2>
-           
+    {chatOpen && (
+      <div className="container">
+        <div className="chat-popup">
+          
+          {/* Header */}
+          <div className="chat-header">
+            <div className="header-info">
+              <Chatbot_icon />
+              <h2 className='logo'>Chatbot</h2>
+            </div>
+            <button className="material-icons">keyboard_arrow_down</button>
           </div>
-          <button class="material-icons">
-             keyboard_arrow_down
-             </button>
-          </div>
-          {/* CHATBOT BODY */}
 
+          {/* Body */}
           <div className="chatbot-body">
             <div className="msg bot-msg">
-              <Chatbot_icon></Chatbot_icon>
-              <p className='msg-text'>Hey there,👋 <br /> How can I help you today ?</p>
+              <Chatbot_icon />
+              <p className='msg-text'>
+                Hey there,👋 <br /> How can I help you today ?
+              </p>
             </div>
-            {chatHistory.map((chat,index)=>(
-              <ChatMessage key = {index} chat = {chat}></ChatMessage>
+
+            {chatHistory.map((chat, index) => (
+              <ChatMessage key={index} chat={chat} />
             ))}
 
-           
+            {/* Invisible scroll target */}
+            <div ref={messageEndRef} />
           </div>
 
-        
-        {/* CHATBOT-FOOTER */}
-        <div className="chatbot-footer">
-         <Chatfooter setChatHistory = {setChatHistory}></Chatfooter>
+          {/* Footer */}
+          <div className="chatbot-footer">
+            <Chatfooter setChatHistory={setChatHistory} />
+          </div>
+
         </div>
       </div>
-     </div>
-    </div>
-  )
+    )}
+  </>
+)
+
 }
 
 export default App
